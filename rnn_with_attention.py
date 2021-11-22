172afa146d555ee8b825d593b06834acfdc34c1a
fuckyouNoob <Genji.1231@outlook.com>
2021-11-23 00:39:36 +0800

    change lstm to seq2seq

--- rnn_with_attention.py
+++ rnn_with_attention.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn.functional as F
 from torch import nn
 from  bigdl.chronos.data.repo_dataset import get_public_dataset
 from torch.utils.data import DataLoader, TensorDataset
@@ -13,7 +14,7 @@ def get_tsdata():
     for tsdata in [tsdata_train, tsdata_val, tsdata_test]:
         tsdata.gen_dt_feature(one_hot_features=("WEEK","HOUR"))\
               .scale(stand, fit=tsdata is tsdata_train)\
-              .roll(lookback=10, horizon=1)
+              .roll(lookback=10, horizon=2)
     return tsdata_train, tsdata_val, tsdata_test
 
 tsdata_train, tsdata_val, tsdata_test = get_tsdata()
@@ -24,26 +25,32 @@ def get_data(data):
                       shuffle=True, batch_size=32)
 
 # prepare model
-class Attention(nn.Module):
-    def __init__(self, input_dim, hidden_dim, num_layers, dropout, output_dim):
-        super().__init__()
+class Seq2Seq(nn.Module):
+    def __init__(self, input_dim, hidden_dim, num_layers, dropout, output_dim, future_seq_len):
+        super(Seq2Seq, self).__init__()
         self.input_dim = input_dim
         self.hidden_dim = hidden_dim
         self.num_layers = num_layers
         self.output_dim = output_dim
-        self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, dropout=dropout, batch_first=True)
-        self.fc = nn.Linear(input_dim, output_dim)
-    
-    def forward(self, input_size):
-        h0 = torch.randn(self.num_layers,input_size.size(0),self.hidden_dim).to(device)
-        c0 = torch.randn(self.num_layers,input_size.size(0),self.hidden_dim).to(device)
-        input_size, _ = self.lstm(input_size, (h0, c0))
-        out = self.fc(input_size[:, -1, :])
-        out = out.view(out.shape[0], 1, out.shape[1])
-        return out
+        self.encoder_gru = nn.GRU(input_dim, hidden_dim, num_layers, dropout=dropout, batch_first=True)
+        self.decoder_gru = nn.GRU(output_dim, hidden_dim, num_layers, dropout=dropout, batch_first=True)
+        self.fc = nn.Linear(hidden_dim, output_dim)
+        self.future_seq_len = future_seq_len
 
+    def forward(self, inq):
+        _, hidden = self.encoder_gru(inq)
+        decoder_inq = inq[:, -1, :self.output_dim]
+        decoder_inq = decoder_inq.unsqueeze(1)
+        decoder_output = []
+        for _ in range(self.future_seq_len):
+            decoder_output_step, _ = self.decoder_gru(decoder_inq, hidden)
+            out_step = self.fc(decoder_output_step)
+            decoder_output.append(out_step)
+            decoder_inq = out_step
+        decoder_output = torch.cat(decoder_output, dim=1)
+        return decoder_output
 
-model = Attention(input_dim=32, hidden_dim=32, num_layers=2, dropout=0.1, output_dim=1).to(device=device)
+model = Seq2Seq(input_dim=32, hidden_dim=32, num_layers=2, dropout=0.1, output_dim=1, future_seq_len=2).to(device=device)
 loss_fn = torch.nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 
@@ -75,8 +82,8 @@ def test_loop(data, model, loss):
     print(f"Avg loss:{test_loss:.4f}")
         
 
-for i in range(10):
-    print(f"No {i} epoch start.")
-    train_loop(tsdata_train, model, loss_fn, optimizer)
+# for i in range(10):
+    # print(f"No {i} epoch start.")
+train_loop(tsdata_train, model, loss_fn, optimizer)
 print('train completed.')
 test_loop(tsdata_test, model, loss_fn)
